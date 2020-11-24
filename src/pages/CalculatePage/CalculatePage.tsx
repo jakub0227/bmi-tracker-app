9352dc66e71d88f609e1745375f5519df5697879
jakub0227 <68559474+jakub0227@users.noreply.github.com>
2020-11-24 19:08:32 +0100

    set Layout, UI boilerplate, Routes, Pages

--- src/pages/CalculatePage/CalculatePage.tsx
+++ src/pages/CalculatePage/CalculatePage.tsx
@@ -1,8 +1,18 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
-import {Paper, useTheme, Container, Typography, TextField, FormControl, Button} from "@material-ui/core";
-import React from 'react';
+import {
+    Box,
+    Paper,
+    useTheme,
+    Container,
+    Typography,
+    TextField,
+    FormControl,
+    Button,
+    ButtonGroup,
+} from "@material-ui/core";
+import React, {useState} from 'react';
 import {Route} from "../../types/Route";
 
 
@@ -12,7 +22,7 @@ export const CalculatePage: Route = () => {
         root: css`
     flex-direction: column;    
 `,
-        paper: css`
+        paper: css`        
     text-align: center;    
     margin-top: ${theme.spacing(10)}px;
     margin-bottom: ${theme.spacing(10)}px;
@@ -23,27 +33,75 @@ export const CalculatePage: Route = () => {
         titleText: css`
     padding-top: ${theme.spacing(10)}px;
 `,
+        form: css`
+    margin: ${theme.spacing(10)}px auto;
+    padding: ${theme.spacing(2)}px;
+    
+`,
+        buttonBase: css`
+    margin-top: ${theme.spacing(15)}px;
+
+`,
+        buttonCalc: css`
+    
+    background: linear-gradient(135deg, #ffec61 15% , #f321d7);
+    width: 20vh;
+    padding: ${theme.spacing(1.2)}px;
+`,
         buttonReset: css`
-    margin-top: ${theme.spacing(10)}px;
+    
     background: linear-gradient(135deg, #8a0185 15% , #64b5f6 80% );
     width: 20vh;
+`,
+        valueBMI: css`
+    margin-top: ${theme.spacing(5)}px;
+    padding: ${theme.spacing(1.5)}px;
 `
-
     }
+// ***** Parameters States *****
+
+//     const [height, setHeight] = useState(0)
+//     const [weight, setWeight] = useState(0)
+
+// ***** Calculations & formulas *****
+
+    // const calculateBMI = () => {
+    //     let bmi = ((weight / height / height) * 10000).toFixed(2);
+    // }
+
+// ***** BMI categories & alert class *****
+
+    //
+    // const getBmiClass = (bmi) => {
+    //     if(bmi < 18.5) return 'Underweight'; // + snackbar alert error
+    //     if(bmi >= 18.5 && bmi <= 24.9) return 'Normal'; // + snackbar alert success
+    //     if(bmi >= 25 && bmi <= 29.9) return 'Overweight'; // + snackbar alert error
+    //     if(bmi >= 30) return 'Obese'; // + snackbar alert ultra-error
+    // }
+
     return (
         <Container css={styles.root}>
             <Paper css={styles.paper} elevation={5}>
-                <Typography css={styles.titleText} variant="h4">Hello from BMI calculator !</Typography>
-                <form>
+                <Typography css={styles.titleText} variant="h4">Hello from BMI Tracker !</Typography>
+                <form css={styles.form}>
                     <FormControl size="medium">
-                        <Typography variant="h6">Height [cm]</Typography>
-                        <TextField label="Type your height" variant="outlined"/>
-                        <Typography variant="h6">Weight [kg]</Typography>
-                        <TextField label="Type your weight" variant="outlined"/>
+                        <Typography variant="h6">Height [ cm ]</Typography>
+                        <TextField label="Type your height" variant="outlined" required>
+                        </TextField>
+                        <Typography variant="h6">Weight [ kg ]</Typography>
+                        <TextField label="Type your weight" variant="outlined" required>
+                        </TextField>
                     </FormControl>
                 </form>
-
-                <Button css={styles.buttonReset} variant="contained" size="large" color="inherit">Reset</Button>
+                <Box css={styles.valueBMI}>
+                    <Typography variant="h5">BMI value :</Typography>
+                </Box>
+                <ButtonGroup css={styles.buttonBase}>
+                    <Button css={styles.buttonCalc} variant="contained"
+                            color="inherit">Calculate BMI</Button>
+                    <Button css={styles.buttonReset} variant="contained"
+                            color="inherit">Reset</Button>
+                </ButtonGroup>
             </Paper>
         </Container>
     );
--- src/pages/DietPage/DietPage.tsx
+++ src/pages/DietPage/DietPage.tsx
@@ -1,16 +1,38 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
-import React from 'react';
+import React, {useState} from 'react';
 import {Route} from "../../types/Route";
-import {Container, Paper, Typography, useTheme} from "@material-ui/core";
+import {
+    Card,
+    Avatar,
+    Icon,
+    CardHeader,
+    Container,
+    CardMedia,
+    CardContent,
+    Collapse,
+    Paper,
+    Typography,
+    useTheme
+} from "@material-ui/core";
 
 
 export const DietPage: Route = props => {
+
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(prev => !prev);
+    }
+
     const theme = useTheme();
     const styles = {
         root: css`
+    display: flex;
     flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;      
 `,
         paper: css`    
     text-align: center;    
@@ -22,7 +44,13 @@ export const DietPage: Route = props => {
 `,
         titleText: css`
     padding-top: ${theme.spacing(10)}px;
-
+`,
+        card: css`
+    text-align: justify;    
+`,
+        cardImg: css`
+    width: 50%;
+    height: 50%;    
 `
     }
 
@@ -30,6 +58,59 @@ export const DietPage: Route = props => {
         <Container css={styles.root}>
             <Paper css={styles.paper} elevation={5}>
                 <Typography css={styles.titleText} variant="h4">We will help you stay healthy !</Typography>
+                <Card css={styles.card}>
+                    <CardHeader
+                        avatar={
+                            <Avatar variant="rounded">
+                                BMI
+                            </Avatar>
+                        }
+                        action={
+                            <Icon onClick={handleExpandClick}>
+                                expand_more
+                            </Icon>
+                        }
+                        title="BMI & Diet recommendations"
+                    />
+                    <CardMedia css={styles.cardImg}
+                               component="img"
+                               src="./assets/bmi.jpg"
+                               title="BMI 1"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" component="p">
+                            This impressive paella is a perfect healthy dish and a fun meal to cook together with your
+                            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                        </Typography>
+                    </CardContent>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>Method:</Typography>
+                            <Typography paragraph>
+                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                                minutes.
+                            </Typography>
+                            <Typography paragraph>
+                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                            </Typography>
+                            <Typography paragraph>
+                                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                                again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                                minutes more. (Discard any mussels that don’t open.)
+                            </Typography>
+                            <Typography>
+                                Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
             </Paper>
         </Container>
     );
